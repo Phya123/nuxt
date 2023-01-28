36e94d622a0c5f816734036850c646a3fe425594
Phya123 <90482389+Phya123@users.noreply.github.com>
2023-01-28 03:58:06 -0500

    Update app.vue

--- examples/advanced/jsx/app.vue
+++ examples/advanced/jsx/app.vue
@@ -1,6 +1,6 @@
 <script lang="tsx" setup>
 // Component could be a simple function with JSX syntax
-const Welcome = () => <span>Welcome </span>
+const Welcome = () => <span>the future</span>
 
 // Or using defineComponent setup that returns render function with JSX syntax
 const Nuxt3 = defineComponent(() => {
@@ -10,9 +10,9 @@ const Nuxt3 = defineComponent(() => {
 // We can combine components with JSX syntax too
 const InlineComponent = () => (
     <div>
-      <Welcome />
+      < />
       <span>to </span>
-      <Nuxt3 />
+      < />ai-trading-app.git 
     </div>
 )
 </script>
